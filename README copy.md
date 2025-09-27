51ce7702bbe78c5c170f8fca816cdb0890d3daf6
judoittt <your_email@example.com>
2025-09-27 21:42:02 +0700

    change 45

--- README copy.md
+++ README copy.md
@@ -10,52 +10,47 @@ Fork dự án **NodeJS-Authentication-System** từ link:
 
 1. **Tạo Captcha**
 
-![1758905040772](image/READMEcopy/1758905040772.png)
+![1758905040772](public/image/captcha1.png)
 
 Sau đó chọn Submit
 
-![1758905064852](image/READMEcopy/1758905064852.png)
+![1758905064852](public/image/captcha2.png)
 
 Google cấp
 
-![1758908911564](image/READMEcopy/1758908911564.png)
+![1758908911564](public/image/captcha3.png)
 
 2. **Tạo Mailtrap**
 
-![1758910590476](image/READMEcopy/1758910590476.png)
+![1758910590476](public/image/mail-trap.png)
 
 Chọn Node.js
 
-![1758910647833](image/READMEcopy/1758910647833.png)
+![1758910590476](public/image/mail-trap1.png)
 
-**3. Cấu hình Google OAuth:** 
+**3. Cấu hình Google OAuth:**
 
+![1758964923515](public/image/oAuth1.png)
 
-![1758964923515](image/READMEcopy/1758964923515.png)
+![1758964930325](public/image/oAuth2.png)
 
-![1758964930325](image/READMEcopy/1758964930325.png)
+![1758964951368](public/image/oAuth3.png)
 
-![1758964951368](image/READMEcopy/1758964951368.png)
+![1758964975628](public/image/oAuth4.png)
 
-![1758964975628](image/READMEcopy/1758964975628.png)
+![1758964988918](public/image/oAuth5.png)
 
-![1758964988918](image/READMEcopy/1758964988918.png)
+![1758965000647](public/image/oAuth6.png)
 
-![1758965000647](image/READMEcopy/1758965000647.png)
-
-4. Tạo file .env:
-
-![1758964740380](image/READMEcopy/1758964740380.png)![1758964740380](image/READMEcopy/1758964740380.png)
+4. Tạo file .env:![env](public/image/env.png)
 
 #### b. Hiển thị **mã số sinh viên** và **họ tên đầy đủ** của bạn trên tất cả các trang.
 
 1. File app.js thêm ngay sau phần cấu hình middleware, để hiện thị mssv + ten
-
-![1758904276666](image/READMEcopy/1758904276666.png)
-
+   ![1758904317010](public/image/showID_Name.png)
 2. File layout.ejs thêm footer để hiện tên
 
-![1758904317010](image/READMEcopy/1758904317010.png)
+![1758904317010](public/image/showID_Name1.png)
 
 #### c. Kiểm thử tất cả các trang theo đúng chức năng của chúng.
 
@@ -64,23 +59,23 @@ Chọn Node.js
    * Truy cập: `http://localhost:3000/user/signup`
    * Nhập thông tin user mới: email: "phuchandsome@gmail.com" ;  pasword: "12345"
 
-   ![1758904605262](image/READMEcopy/1758904605262.png)
+   ![1758904605262](public/image/signUp.png)
 2. **Trang Sign in (Đăng nhập)**
 
    * Truy cập: `http://localhost:3000/user/signin`
    * Login với tài khoản vừa tạo: email:  "phuchandsome@gmail.com" ;  pasword: "12345"
 
-   ![1758908953898](image/READMEcopy/1758908953898.png)
+   ![1758908953898](public/image/signIn.png)
 3. **Trang Homepage (Profile)**
 
    * Truy cập: `http://localhost:3000/user/homepage`
 
-     ![1758909504917](image/READMEcopy/1758909504917.png)
+   ![1758909504917](public/image/homepage.png)
 4. **Trang Sign out (Đăng xuất)**
 
    * Truy cập: `http://localhost:3000/user/signout`
 
-     ![1758909537033](image/READMEcopy/1758909537033.png)
+   ![1758909537033](public/image/sign-out.png)
 5. **Trang Forgot Password (Quên mật khẩu)**
 
    * Truy cập: `http://localhost:3000/user/forgot-password`
@@ -88,111 +83,98 @@ Chọn Node.js
 
    Nhập email đã quên mật khẩu
 
-   ![1758909972080](https://file+.vscode-resource.vscode-cdn.net/d%3A/%5B3%5D%20N%C4%83m%204_1/%5B3%5D%20LapTrinhHDV/%5B1%5D%20ThucHanh/lab6/src/src/NodeJS-Authentication-System/image/READMEcopy/1758909972080.png)
+   ![1758909972080](public/image/forgot-password.png)
 
    - Hệ thống gửi mail và mật khẩu mới, giả lập trên MailTrap
 
-   ![1758910974978](https://file+.vscode-resource.vscode-cdn.net/d%3A/%5B3%5D%20N%C4%83m%204_1/%5B3%5D%20LapTrinhHDV/%5B1%5D%20ThucHanh/lab6/src/src/NodeJS-Authentication-System/image/READMEcopy/1758910974978.png)
+   ![1758909972080](public/image/forgot-password1.png)
 
    Mật khẩu được gửi giả lập vào Mail Train
 
-   ![1758911037466](image/READMEcopy/1758911037466.png)
+   ![1758911037466](public/image/mail-trap2.png)
 
    Password Reset
 
-   ![1758911065448](image/READMEcopy/1758911065448.png)
-
-Nhập lại Email và Mật khẩu mới
-
-![1758911123321](image/READMEcopy/1758911123321.png)
-
+   ![1758911065448](public/image/mail-trap3.png)
 6. **Trang Change Password (Đổi mật khẩu)**
 
-* Truy cập: `http://localhost:3000/user/change-password
+* Truy cập: `http://localhost:3000/user/change-password. Đổi mật khẩu ban đầu password="12345" -> "1234567"
 
-  Đổi mật khẩu ban đầu password="12345" -> "1234567"
-
-  ![1758909323899](image/READMEcopy/1758909323899.png)
+  ![1758909323899](public/image/change-password.png)
 
 **7. Login bằng Google (OAuth2)**
 
 * Truy cập: `http://localhost:3000/auth/google`
-  B1. Sign in with Google
 
-  ![1758965042320](image/READMEcopy/1758965042320.png)
-* B2. Chọn tài khoản:
+  * B1. Sign in with Google
 
-![1758965252697](image/READMEcopy/1758965252697.png)
+    ![1758965042320](public/image/sign-in-gg.png)
+  * B2. Chọn tài khoản:
 
-* B3. Chọn tiếp tục
+    ![1758965042320](public/image/sign-in-gg1.png)
+  * B3. Chọn tiếp tục
 
-![1758965092286](image/READMEcopy/1758965092286.png)
-
+    ![1758965042320](public/image/sign-in-gg2.png)
 
 #### d. Chụp màn hình toàn bộ kết quả kiểm thử, đặt tên file theo chức năng kiểm thử.
 
 1. Sign up: User created successfully
-
-![1758904666833](image/READMEcopy/1758904666833.png)
+   ![1758904666833](public/results/signUp.png)
 
 Database:
 
-![1758904708643](image/READMEcopy/1758904708643.png)
+![1758904708643](public/results/signUpDB.png)
 
 2. Sign In: email: "phuchandsome@gmail.com" ;  pasword: "12345"
 
-![1758909043484](image/READMEcopy/1758909043484.png)
+![1758909043484](public/results/signIn.png)
 
 Sign In thành công hiện trang HomePage
 
-![1758909014499](image/READMEcopy/1758909014499.png)
+![1758909014499](public/results/signIn-successfull.png)
 
 3. Change Password: password: "12345" - > "1234567"
 
-![1758909649120](image/READMEcopy/1758909649120.png)
-
-Password changed succefully
-
-![1758909387975](image/READMEcopy/1758909387975.png)
+![1758909649120](public/results/change-password-enter.png)
 
 Database:
 
-![1758909434830](image/READMEcopy/1758909434830.png)
+![1758904666833](public/results/change-password-DB.png)
 
 4. Sign out: thành công hiện trang Sign In
 
-![1758909798596](image/READMEcopy/1758909798596.png)
+![1758909798596](public/results/log-out.png)
 
 5. Forgot Password: dùng MailTrap giả lập khi nhập email muốn lấy lại mật khẩu, tin nhắn MẬT KHẨU MỚI sẽ gửi lên mail trap
 
 Nhập lại Email và Mật khẩu mới
 
-![1758911123321](image/READMEcopy/1758911123321.png)
+![1758911123321](public/results/forgot-password.png)
+
+Nhập mật khẩu
+
+![1758909649120](public/results/forgot-password-email.png)
 
 Đổi thành công:
 
-![1758911157236](image/READMEcopy/1758911157236.png)
+![1758911157236](public/image/homepage.png)
 
 Database
 
-![1758911142625](image/READMEcopy/1758911142625.png)
+![1758909649120](public/results/forgot-passwordDB.png)
 
 6. Sign In With Google
 
-   ![1758965310416](image/READMEcopy/1758965310416.png)
+   ![1758965310416](public/image/sign-in-gg.png)
 
- Đăng nhập thành công. Hiển thị Homepage
+   Đăng nhập thành công. Hiển thị Homepage
 
-![1758965129698](image/READMEcopy/1758965129698.png)
+   ![1758965129698](public/results/sign-in-gg1.png)
 
-Database:
+   ![1758909649120](public/results/sign-in-gg.png)
+   Database:
 
-![1758965184114](image/READMEcopy/1758965184114.png)
-
-Ví dụ:
-
-* `login.png`
-* `logout.jpg`
+![1758965184114](public/results/sign-in-gg-DB.png)
 
 Sau đó, đặt tất cả hình ảnh vào thư mục  **public/results** .
 
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,139 +1,207 @@
-  import mongoose from "mongoose";
-  import User from "../models/userModel.js";
-  import bcrypt from "bcrypt";
-  import { transporter } from "../config/nodemailerConfig.js";
-  import dotenv from "dotenv";
-  import fetch from "node-fetch";
-  dotenv.config();
+import mongoose from "mongoose";
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import { transporter } from "../config/nodemailerConfig.js";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+dotenv.config();
 
-  export class UserGetController {
-    getSignUpPage = (req, res) => {
-      res.render("signup", { message: "", siteKey: process.env.RECAPTCHA_SITE_KEY });
-    };
-
-    getSignInPage = (req, res) => {
-      res.render("signin", { message: "", siteKey: process.env.RECAPTCHA_SITE_KEY });
-    };
-
-    homePage = (req, res) => {
-      const email = req.session.userEmail;
-      if (!email) {
-        return res.status(404).render("signin", { 
-          message: "Please sign in to view the homepage", 
-          siteKey: process.env.RECAPTCHA_SITE_KEY 
-        });
-      }
-      res.render("homepage");
-    };
-
-    getForgotPassword = (req, res) => {
-    res.render("forgot-password", { message: "", siteKey: process.env.RECAPTCHA_SITE_KEY });
-  };
-
-
-    getChangePassword = (req, res) => {
-    const email = req.session.userEmail;
-    if (!email) {
-      return res.status(404).render("signin", { 
-        message: "Please sign in to change the password", 
-        siteKey: process.env.RECAPTCHA_SITE_KEY 
-      });
-    }
-    res.render("change-password", { 
-      message: "", 
-      siteKey: process.env.RECAPTCHA_SITE_KEY 
+export class UserGetController {
+  getSignUpPage = (req, res) => {
+    res.render("signup", {
+      message: "",
+      siteKey: process.env.RECAPTCHA_SITE_KEY,
     });
   };
 
+  getSignInPage = (req, res) => {
+    res.render("signin", {
+      message: "",
+      siteKey: process.env.RECAPTCHA_SITE_KEY,
+    });
+  };
 
-    logoutUser = (req, res) => {
-      req.session.destroy((err) => {
-        if (err) {
-          console.error("Error signing out:", err);
-          res.status(500).send("Error signing out");
-        } else {
-          res.status(201).render("signin", { 
-            message: "User logout", 
-            siteKey: process.env.RECAPTCHA_SITE_KEY 
-          });
-        }
+  homePage = (req, res) => {
+    const email = req.session.userEmail;
+    if (!email) {
+      return res.status(404).render("signin", {
+        message: "Please sign in to view the homepage",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
       });
-    };
-  }
+    }
+    res.render("homepage");
+  };
 
-  export class UserPostController {
-    // 🔹 Sign up
-    createUser = async (req, res) => {
-      const { username, email, password, cpassword } = req.body;
-      if (password !== cpassword) {
-        return res.status(400).render("signup", { 
-          message: "Passwords don't match", 
-          siteKey: process.env.RECAPTCHA_SITE_KEY 
+  getForgotPassword = (req, res) => {
+    res.render("forgot-password", {
+      message: "",
+      siteKey: process.env.RECAPTCHA_SITE_KEY,
+    });
+  };
+
+  getChangePassword = (req, res) => {
+    const email = req.session.userEmail;
+    if (!email) {
+      return res.status(404).render("signin", {
+        message: "Please sign in to change the password",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
+      });
+    }
+    res.render("change-password", {
+      message: "",
+      siteKey: process.env.RECAPTCHA_SITE_KEY,
+    });
+  };
+
+  logoutUser = (req, res) => {
+    req.session.destroy((err) => {
+      if (err) {
+        console.error("Error signing out:", err);
+        res.status(500).send("Error signing out");
+      } else {
+        res.status(201).render("signin", {
+          message: "User logout",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
+        });
+      }
+    });
+  };
+}
+
+export class UserPostController {
+  // 🔹 Sign up
+  createUser = async (req, res) => {
+    const { username, email, password, cpassword } = req.body;
+    const recaptcha = req.body["g-recaptcha-response"];
+
+    // ✅ Check captcha trước
+    if (!recaptcha) {
+      return res.status(400).render("signup", {
+        message: "Please select captcha",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
+      });
+    }
+
+    const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptcha}`;
+
+    try {
+      // Gửi request verify tới Google
+      const googleResponse = await fetch(url, { method: "POST" });
+      const data = await googleResponse.json();
+
+      if (!data.success) {
+        return res.status(400).render("signup", {
+          message: "Captcha verification failed",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
         });
       }
 
+      // ✅ Captcha OK → kiểm tra mật khẩu
+      if (password !== cpassword) {
+        return res.status(400).render("signup", {
+          message: "Passwords don't match",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
+        });
+      }
+
+      // ✅ Check email tồn tại
       const existingUser = await User.findOne({ email });
       if (existingUser) {
-        return res.status(400).render("signup", { 
-          message: "User already exists", 
-          siteKey: process.env.RECAPTCHA_SITE_KEY 
+        return res.status(400).render("signup", {
+          message: "User already exists",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
         });
       }
 
+      // ✅ Hash password
       const hashedPassword = await bcrypt.hash(password, 10);
       const newUser = new User({ username, email, password: hashedPassword });
 
-      try {
-        await newUser.save();
-        res.status(201).render("signin", { 
-          message: "User created successfully", 
-          siteKey: process.env.RECAPTCHA_SITE_KEY 
+      await newUser.save();
+
+      // ✅ Đăng ký thành công → render Sign In
+      return res.status(201).render("signin", {
+        message: "User created successfully",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
+      });
+    } catch (error) {
+      console.error("Sign up error:", error);
+      return res.status(500).render("signup", {
+        message: "Internal server error",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
+      });
+    }
+  };
+
+  // 🔹 Sign in
+  signInUser = async (req, res) => {
+    const { email, password } = req.body;
+    const recaptcha = req.body["g-recaptcha-response"];
+
+    if (!recaptcha) {
+      return res
+        .status(400)
+        .render("signin", {
+          message: "Please select captcha",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
         });
-      } catch (error) {
-        res.status(409).json({ message: error.message });
+    }
+
+    // Verify reCAPTCHA
+    const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptcha}`;
+
+    try {
+      const googleResponse = await fetch(url, { method: "POST" });
+      const data = await googleResponse.json();
+
+      if (!data.success) {
+        return res
+          .status(400)
+          .render("signin", {
+            message: "Captcha verification failed",
+            siteKey: process.env.RECAPTCHA_SITE_KEY,
+          });
       }
-    };
 
-    // 🔹 Sign in
-    signInUser = async (req, res) => {
-      const { email, password } = req.body;
-      const recaptcha = req.body["g-recaptcha-response"];
-
-      if (!recaptcha) {
-        return res.status(400).render("signin", { message: "Please select captcha", siteKey: process.env.RECAPTCHA_SITE_KEY });
+      // Sau khi captcha OK → kiểm tra user
+      const existingUser = await User.findOne({ email });
+      if (!existingUser) {
+        return res
+          .status(404)
+          .render("signin", {
+            message: "User doesn't exist",
+            siteKey: process.env.RECAPTCHA_SITE_KEY,
+          });
       }
 
-      // Verify reCAPTCHA
-      const secretKey = process.env.RECAPTCHA_SECRET_KEY;
-      const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptcha}`;
-
-      try {
-        const googleResponse = await fetch(url, { method: "POST" });
-        const data = await googleResponse.json();
-
-        if (!data.success) {
-          return res.status(400).render("signin", { message: "Captcha verification failed", siteKey: process.env.RECAPTCHA_SITE_KEY });
-        }
-
-        // Sau khi captcha OK → kiểm tra user
-        const existingUser = await User.findOne({ email });
-        if (!existingUser) {
-          return res.status(404).render("signin", { message: "User doesn't exist", siteKey: process.env.RECAPTCHA_SITE_KEY });
-        }
-
-        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
-        if (!isPasswordCorrect) {
-          return res.status(400).render("signin", { message: "Invalid credentials || Incorrect Password", siteKey: process.env.RECAPTCHA_SITE_KEY });
-        }
-
-        req.session.userEmail = email;
-        return res.redirect("/user/homepage");
-      } catch (error) {
-        console.error("Sign in error:", error);
-        return res.status(500).render("signin", { message: "Internal server error", siteKey: process.env.RECAPTCHA_SITE_KEY });
+      const isPasswordCorrect = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+      if (!isPasswordCorrect) {
+        return res
+          .status(400)
+          .render("signin", {
+            message: "Invalid credentials || Incorrect Password",
+            siteKey: process.env.RECAPTCHA_SITE_KEY,
+          });
       }
-    };
-     // 🔹 Sign in with Google
+
+      req.session.userEmail = email;
+      return res.redirect("/user/homepage");
+    } catch (error) {
+      console.error("Sign in error:", error);
+      return res
+        .status(500)
+        .render("signin", {
+          message: "Internal server error",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
+        });
+    }
+  };
+  // 🔹 Sign in with Google
   googleSignInSuccess = async (req, res) => {
     try {
       if (!req.user || !req.user._json) {
@@ -142,7 +210,9 @@
 
       const { email, name, sub } = req.user._json;
       if (!email) {
-        return res.status(403).json({ error: true, message: "Email not found in Google profile" });
+        return res
+          .status(403)
+          .json({ error: true, message: "Email not found in Google profile" });
       }
 
       // check DB
@@ -167,16 +237,15 @@
     res.status(401).json({ error: true, message: "Google login failed" });
   };
 
-
-    // 🔹 Forgot password
+  // 🔹 Forgot password
   forgotPassword = async (req, res) => {
     const { email } = req.body;
     const recaptcha = req.body["g-recaptcha-response"];
 
     if (!recaptcha) {
-      return res.status(400).render("forgot-password", { 
-        message: "Please select captcha", 
-        siteKey: process.env.RECAPTCHA_SITE_KEY 
+      return res.status(400).render("forgot-password", {
+        message: "Please select captcha",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
       });
     }
 
@@ -188,18 +257,18 @@
       const data = await googleResponse.json();
 
       if (!data.success) {
-        return res.status(400).render("forgot-password", { 
-          message: "Captcha verification failed", 
-          siteKey: process.env.RECAPTCHA_SITE_KEY 
+        return res.status(400).render("forgot-password", {
+          message: "Captcha verification failed",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
         });
       }
 
       // Nếu captcha OK → check user
       const existingUser = await User.findOne({ email });
       if (!existingUser) {
-        return res.status(404).render("forgot-password", { 
-          message: "User doesn't exist", 
-          siteKey: process.env.RECAPTCHA_SITE_KEY 
+        return res.status(404).render("forgot-password", {
+          message: "User doesn't exist",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
         });
       }
 
@@ -208,43 +277,43 @@
 
       try {
         await transporter.sendMail({
-          from: process.env.EMAIL_FROM,   // ✅ dùng Mailtrap config
+          from: process.env.EMAIL_FROM, // ✅ dùng Mailtrap config
           to: email,
           subject: "Password Reset",
           text: `Your new password is: ${newPassword}`,
         });
       } catch (error) {
         console.log(error);
-        return res.status(500).render("forgot-password", { 
-          message: "Email sending failed: " + error.message, 
-          siteKey: process.env.RECAPTCHA_SITE_KEY 
+        return res.status(500).render("forgot-password", {
+          message: "Email sending failed: " + error.message,
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
         });
       }
 
       existingUser.password = hashedPassword;
       await existingUser.save();
 
-      res.status(201).render("signin", { 
-        message: "✅ New Password sent to your email (check Mailtrap inbox)", 
-        siteKey: process.env.RECAPTCHA_SITE_KEY 
+      res.status(201).render("signin", {
+        message: "✅ New Password sent to your email (check Mailtrap inbox)",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
       });
     } catch (error) {
-      res.status(500).render("forgot-password", { 
-        message: error.message, 
-        siteKey: process.env.RECAPTCHA_SITE_KEY 
+      res.status(500).render("forgot-password", {
+        message: error.message,
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
       });
     }
   };
 
-    // change password
+  // change password
   changePassword = async (req, res) => {
     const { oldPassword, newPassword } = req.body;
     const recaptcha = req.body["g-recaptcha-response"];
 
     if (!recaptcha) {
-      return res.status(400).render("change-password", { 
-        message: "Please select captcha", 
-        siteKey: process.env.RECAPTCHA_SITE_KEY 
+      return res.status(400).render("change-password", {
+        message: "Please select captcha",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
       });
     }
 
@@ -256,33 +325,40 @@
       const data = await googleResponse.json();
 
       if (!data.success) {
-        return res.status(400).render("change-password", { 
-          message: "Captcha verification failed", 
-          siteKey: process.env.RECAPTCHA_SITE_KEY 
+        return res.status(400).render("change-password", {
+          message: "Captcha verification failed",
+          siteKey: process.env.RECAPTCHA_SITE_KEY,
         });
       }
 
       const email = req.session.userEmail;
       const existingUser = await User.findOne({ email });
       if (!existingUser) {
-        return res.status(404).render("change-password", { message: "User doesn't exist" });
+        return res
+          .status(404)
+          .render("change-password", { message: "User doesn't exist" });
       }
 
-      const isPasswordCorrect = await bcrypt.compare(oldPassword, existingUser.password);
+      const isPasswordCorrect = await bcrypt.compare(
+        oldPassword,
+        existingUser.password
+      );
       if (!isPasswordCorrect) {
-        return res.status(400).render("change-password", { message: "Invalid credentials" });
+        return res
+          .status(400)
+          .render("change-password", { message: "Invalid credentials" });
       }
 
       const hashedPassword = await bcrypt.hash(newPassword, 10);
       existingUser.password = hashedPassword;
       await existingUser.save();
 
-      res.status(201).render("signin", { 
-        message: "Password changed successfully", 
-        siteKey: process.env.RECAPTCHA_SITE_KEY 
+      res.status(201).render("signin", {
+        message: "Password changed successfully",
+        siteKey: process.env.RECAPTCHA_SITE_KEY,
       });
     } catch (error) {
       res.status(500).render("change-password", { message: error.message });
     }
   };
-  }
+}
